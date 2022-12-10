af3a699d3b3c62aef24f33a43b100aa712e13fda
Anton Ishchenko <anton.ishchenko@ukg.com>
2022-12-10 22:34:20 +0200

    refactoring for monitoring

--- components/storage-service/src/main/java/com/example/storageservice/StorageServiceApplication.java
+++ components/storage-service/src/main/java/com/example/storageservice/StorageServiceApplication.java
@@ -1,12 +1,19 @@
 package com.example.storageservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StorageServiceApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(StorageServiceApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("application starting");
+		logger.error("application starting");
+		logger.debug("application starting");
 		SpringApplication.run(StorageServiceApplication.class, args);
 	}
 
